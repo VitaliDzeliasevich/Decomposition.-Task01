d238ec5c2392f6bd709153f1b2c828ff5b46923c
VitaliDzeliasevich <vitali_delesevich@tut.by>
2021-10-09 13:59:49 +0300

    Task09.10.21

---
+++ CompositionTask/src/test/java/Task7CompositionTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+public class Task7CompositionTest {
+
+    @org.junit.Test
+    public void calculateStep() {
+        int actual = Task7Composition.calculateStep(40,80);
+        int expected = 4;
+        assertEquals(expected,actual);
+    }
+
+    @org.junit.Test
+    public void calculateX() {
+        double actual = Task7Composition.calculateX(20);
+        double expected = 0.6669;
+        assertEquals(expected,actual,0.0001);
+    }
+}
